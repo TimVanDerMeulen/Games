c8ea8ae0a4d0a3586ea68837cb07cfa5a0de89b7
unknown <tim.vdm@t-online.de>
2020-05-25 17:46:56 +0200

    first stuff of new game

---
+++ Dimensions/Assets/Scripts/Platform/GroundController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GroundController : MonoBehaviour
+{
+	[Header("Resources")]
+	public string PLATFORM_PREFAB_FOLDER;
+	
+	/*
+	 Ground Settings
+	*/
+	[Header("Dimensions")]
+	public int maxSize;
+	public int tileSize; // always squared even if tile itself isn't!
+	
+	private GameObject[,] groundField;
+		
+	private UnityEngine.Random random = new UnityEngine.Random();
+	private List<GameObject> platforms;
+
+    void Start()
+    {
+		groundField = new GameObject[maxSize, maxSize];
+    }
+
+    void Update()
+    {
+        
+    }
+	
+	private void createRandomPlatform(){
+		
+	}
+	
+	private void createPlatform(GameObject platform, int x, int z){
+		Instantiate(platform, new Vector3(x * tileSize, 0, z * tileSize), Quaternion.identity);
+	}
+
+}
---
+++ Dimensions/Assets/Scripts/Platform/PlatformController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour, Destructable
+{
+	
+	private Rigidbody rigidbody;
+	
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        rigidbody.AddForce(new Vector3(1,0,1).normalized);
+    }
+	
+	public void Destroy(){
+		
+	}
+	
+}
