a9401746be68d72abce08ddb09735a07dac83e29
unknown <tim.vdm@t-online.de>
2020-05-25 10:13:07 +0200

    added games

---
+++ Blobs/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private GameObject camera;
+
+    public float speed = 0.7F;
+
+    void Start()
+    {
+        rb = GetComponentInChildren<Rigidbody>();
+        camera = transform.Find("Camera").gameObject;
+    }
+
+    void Update()
+    {
+        /*
+        float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.AddForce(movement * transform.rotation. * speed);
+        */
+        float h = Input.GetAxis("Horizontal") * speed;
+        float v = Input.GetAxis("Vertical") * speed;
+
+        //        transform.localPosition += transform.right * h;
+        //        transform.localPosition += transform.forward * v;
+
+        Vector3 RIGHT = camera.transform.TransformDirection(Vector3.right);
+        Vector3 FORWARD = camera.transform.TransformDirection(Vector3.forward);
+
+        RIGHT.y = 0;
+        FORWARD.y = 0;
+
+        transform.localPosition += RIGHT * h;
+        transform.localPosition += FORWARD * v;
+    }
+}
---
+++ Builder/Assets/Scripts/CollisionBroadcaster.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionBroadcaster : MonoBehaviour
+{
+	
+	private List<Subscriber> subscribers = new List<Subscriber>();
+	
+    /*
+	########################## Game Loop ##########################
+	*/
+	
+	public void Start() {
+		//subscribers = new List<Subscriber>();
+    }
+	
+	public void OnCollisionEnter(Collision collision){
+		foreach (Subscriber subscriber in subscribers){
+			subscriber.targetCollisionEnter(gameObject, collision);
+		}
+	}
+	
+	public void OnCollisionExit(Collision collision){
+		foreach (Subscriber subscriber in subscribers){
+			subscriber.targetCollisionExit(gameObject, collision);
+		}
+	}
+	
+	void OnTriggerEnter(Collider collider){
+		foreach (Subscriber subscriber in subscribers){
+			subscriber.targetTriggerEnter(gameObject, collider);
+		}
+	}
+	
+	void OnTriggerExit(Collider collider){
+		foreach (Subscriber subscriber in subscribers){
+			subscriber.targetTriggerExit(gameObject, collider);
+		}
+	}
+	
+	/*
+	########################## Other ##########################
+	*/
+	
+	public interface Subscriber{
+		void targetCollisionEnter(GameObject target, Collision collision);
+		void targetCollisionExit(GameObject target, Collision collision);
+		void targetTriggerEnter(GameObject target, Collider collider);
+		void targetTriggerExit(GameObject target, Collider collider);
+	}
+	
+	public void subscribe(Subscriber subscriber){
+		if(!subscribers.Contains(subscriber))
+			subscribers.Add(subscriber);
+	}
+	
+	public void unsubscribe(Subscriber subscriber){
+		if(subscribers.Contains(subscriber))
+			subscribers.Remove(subscriber);
+	}
+	
+}
