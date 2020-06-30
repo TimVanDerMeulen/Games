28ed82d07eb1fe6ba58c86cca251b45f1b494cd8
unknown <tim.vdm@t-online.de>
2020-06-30 19:18:28 +0200

    new game

---
+++ Instructions/Assets/Scripts/Ground/GroundController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Random=UnityEngine.Random;
+
+public class GroundController : MonoBehaviour
+{
+	private NavMeshSurface navMeshSurface;
+
+    void Start()
+    {
+		navMeshSurface = GetComponent<NavMeshSurface>();
+		
+		UpdateNavMesh();
+    }
+
+    void Update()
+    {
+		
+    }
+	
+	public void UpdateNavMesh(){
+		navMeshSurface.BuildNavMesh();
+		//PlayerController.RefreshNavMeshAgent();	
+	}
+
+}
---
+++ Instructions/Assets/Scripts/Interaction/Interaction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction
+{
+    public GameObject source { get; private set; }
+
+    public InteractionType type { get; private set; }
+
+    public Interaction(InteractionType type, GameObject source)
+    {
+        this.type = type;
+        this.source = source;
+    }
+}
