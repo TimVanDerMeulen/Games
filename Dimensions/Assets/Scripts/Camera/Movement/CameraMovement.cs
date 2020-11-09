5ac5515adabb2b8ebf6f4c202fa2501fb430d308
Tim van der Meulen <meulen@predic8.de>
2020-11-09 18:55:54 +0100

    moved files

--- Dimensions/Assets/Scripts/Camera/Movement/CameraMovement.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface CameraMovement
-{
-    Vector3 GetCurrentCenter();
-	//void SetCurrentCenter(Vector3 center);
-}
--- Dimensions/Assets/Scripts/Mobs/HostileMob.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface HostileMob : Mob
-{
-	string GetName();
-    //float GetHealth();
-}
